603438e706aa1b22c24610b17f80277e68993a18
HanGyeol.Kim <148546746+Kyeol1125@users.noreply.github.com>
2023-11-08 05:02:27 +0900

    Delete AI-MODEL/FRONT-END directory

--- AI-MODEL/FRONT-END/app/src/main/java/com/example/myapplication/Result.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class Result extends Activity {
-    // TextView 참조
-}
